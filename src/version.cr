a767fa80f91558e6e6a9307890b88a509003226f
Kirk Haines <wyhaines@gmail.com>
2022-06-28 08:33:47 -0600

    I namespaced the VERSION into the SDK, but the SDK version is the "OpenTelemetry" library version, so adding an alias back.

--- src/version.cr
+++ src/version.cr
@@ -1,5 +1,9 @@
-module OpenTelemetry::SDK
-  {% begin %}
-  VERSION = {{ read_file("#{__DIR__}/../VERSION").chomp }}
-  {% end %}
+module OpenTelemetry
+  module SDK
+    {% begin %}
+    VERSION = {{ read_file("#{__DIR__}/../VERSION").chomp }}
+    {% end %}
+  end
+
+  alias VERSION = SDK::VERSION
 end
